7d79f13a5353c411f83b974e5bd52b1121ebe5d6
pkiage <paul.r.kiage@gmail.com>
2022-07-13 14:30:02 +0300

    refactor: move files

--- shell-commands/setup-cfd-primer.sh
+++ shell-commands/setup-cfd-primer.sh
@@ -1,5 +1,7 @@
+S3_BucketName = $1
+
 cd .circleci/files
 aws cloudformation deploy \
          --template-file cloudfront.yml \
          --stack-name InitialStack\
-         --parameter-overrides WorkflowID=$1
+         --parameter-overrides WorkflowID=${S3_BucketName}
