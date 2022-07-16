bc48336e04249115ddf78646823d0a24a6077a1d
pkiage <paul.r.kiage@gmail.com>
2022-07-16 11:08:46 +0300

    avoid repeated udapeople- prefix for s3 + remove the prefix from workflow ID export used in cleanup job

--- shell-commands/setup-cloudfront-primer.sh
+++ shell-commands/setup-cloudfront-primer.sh
@@ -1,5 +1,9 @@
 # Bucket Name = $1
-
+# bucket should have udapeople-  prefix
+# in workflowID input what is after the prefix 
+# e.g. if initial bucket is udapeople-tutafaulu 
+# then $1 should be tutafaulu
+# because udapeople- prefix already sorted in .circleci/files/cloudfront.yml file
 cd .circleci/files
 aws cloudformation deploy \
          --template-file cloudfront.yml \
